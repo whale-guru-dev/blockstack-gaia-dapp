04f2038c19fe4f1963a411df568a2dcb251ca176
GOD <masterionic@gmail.com>
2020-03-17 04:46:50 +0800

    cors

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding'
-    }
+    },
+    proxy: 'https://amazing-northcutt-68d4e0.netlify.com/',
   }
 }
